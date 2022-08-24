1cb57f8fe19c800e282c93862f397d41cd2ff1b3
Adkhambek <adkhambekyusupov@gmail.com>
2022-08-24 11:06:09 +0500

    sdfsdfsdf

--- src/components/Header/UI/NavItem.tsx
+++ src/components/Header/UI/NavItem.tsx
@@ -2,6 +2,7 @@ import React, {FC, useCallback, useState} from 'react';
 import styles from "components/Header/Header.module.scss";
 import {ArrowDown} from "static/icons/icon";
 import {ICategories} from "models/index";
+import A from "components/UI/A/A";
 
 interface NavItemProps {
     item: ICategories,
@@ -10,15 +11,22 @@ interface NavItemProps {
 
 const NavItem: FC<NavItemProps> = ({ item, handleShowMenu }) => {
 
-    const [ show, setShow ] = useState<boolean>(false)
-
+    const [ showSubmenu, setShowSubmenu ] = useState<boolean>(false)
     const handleShow = useCallback(() => {
-        setShow(!show)
-    }, [show])
+        setShowSubmenu(!showSubmenu)
+    }, [showSubmenu])
     return (
         <li onClick={handleShow} className={styles.nav_li} key={item?.id}>
-            <button id={`${item?.id}`} className="headerList" onClick={() => handleShowMenu(item.id)}>{item.name}</button>
-                <ArrowDown/>
+            <button id={`${item?.id}`} className="headerList" onClick={() => handleShowMenu(item.id)}>{item.name} <ArrowDown/></button>
+            {
+                showSubmenu && <ul className={styles.navbar__submenu}>
+                    {
+                        item?.childs && item.childs.map(child => {
+                            return <li key={child?.id}><A href={`${child?.name}`}>{child?.name}</A></li>
+                        })
+                    }
+                </ul>
+            }
         </li>
     );
 };
